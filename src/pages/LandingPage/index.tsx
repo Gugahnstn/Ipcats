eb55f6158aba525f0766117e7382c941f7628e73
Gugahnstn <gustavonstn@hotmail.com>
2022-02-27 18:43:24 -0300

    feat: adding style component of the style.ts

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,16 +1,17 @@
 import Header from "../../components/Header"
 import Context from "../../components/Context"
 import Search from "../../components/Search"
+import { Container } from "./style"
 // import CompoMap from "../../components/CompoMap"
 
 const LandingPage = () => {
   return(
-    <>
+    <Container>
       <Header />
       <Context />
       <Search />
       {/* <CompoMap /> */}
-    </>
+    </Container>
   );
 }
 
