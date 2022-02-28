714d874906f4f081d65ae74d577b299ebe0daff5
Gugahnstn <gustavonstn@hotmail.com>
2022-02-28 14:17:02 -0300

    feat: implementing useState and removed useEffect

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,70 +1,82 @@
-import { SearchStyle, SearchText ,SearchInput, ResultSearch } from "./style";
-import { useState, useEffect, ReactHTMLElement} from "react";
-import ServiceIpApi from "../../services/ServiceIpApi";
+import { SearchStyle, SearchText, SearchInput, ResultSearch } from "./style";
+import serviceIpApi from "../../services/services-viacep-api";
+import { useState } from "react";
 
 const Search = () => {
-  const [city, setCity] = useState<string|null>(null);
-  const [estate, setEstate] = useState<string|null>(null);
-  const [query, setQuery] = useState<string|null>(null);
-  const [continent, setContinent] = useState<string|null>(null);
-  const [country, setCountry] = useState<string|null>(null);
-  
-  useEffect(()=>{
-    let searchButton = document.querySelector('.searchButton') as HTMLElement;
-    let search = document.querySelector('.search') as HTMLElement;
-    
-    let consultIpApi = async () => {
-      let ipApi = await ServiceIpApi('' + search);
+  const [continent, setContinent] = useState<string | null>(null);
+  const [country, setCountry] = useState<string | null>(null);
+  const [estate, setEstate] = useState<string | null>(null);
+  const [query, setQuery] = useState<string | null>(null);
+  const [city, setCity] = useState<string | null>(null);
 
-      setCity(ipApi.city);
-      setEstate(ipApi.region);
-      setQuery(ipApi.query);
-      setContinent(ipApi.continent);
-      setCountry(ipApi.country);
-    };
-    
-    searchButton?.addEventListener('click', consultIpApi);
-    search?.addEventListener('keypress', (event: KeyboardEvent): void => {
-      if(event.key == 'enter'){
-        consultIpApi();
-      };
-    });
-  }, []);
+  const [flag, setFlag] = useState<string | null>(null);
+  const [search, setSearch] = useState("");
 
-  return(
+  const apiIp = async () => {
+    const ipApi = await serviceIpApi(search);
+
+    setContinent(ipApi.continent);
+    setFlag(ipApi.countryCode);
+    setCountry(ipApi.country);
+    setEstate(ipApi.region);
+    setQuery(ipApi.query);
+    setCity(ipApi.city);
+  };
+
+  function handleText(event) {
+    setSearch(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    apiIp();
+  }
+
+  return (
     <SearchStyle>
       <SearchText>
         <p>DIGITE O IP/DOMINIO DESEJADO:</p>
       </SearchText>
-      <SearchInput>
-        <input type="text" className="search" placeholder="IP/DOMAINS"/>
-        <input type="button" className="searchButton" value="SEARCH" />
+      <SearchInput onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="search"
+          onChange={handleText}
+          value={search}
+          placeholder="IP/DOMAINS"
+        />
+        <button type="submit" className="searchButton">
+          BUSCAR
+        </button>
       </SearchInput>
       <ResultSearch>
         <hr />
         <div className="cityEstate">
           <h1>Cidade, Estado:</h1>
-          <p>{ !city ? "NENHUM" : `${city.toUpperCase()}-${estate?.toUpperCase()}` }</p>
+          <p>{!city ? "NENHUM" : `${city.toUpperCase()}-${estate}`}</p>
         </div>
         <hr />
         <div className="query">
           <h1>IP/Domain:</h1>
-          <p>{ !query ? "NENHUM" : `${query}`}</p>
+          <p>{!query ? "NENHUM" : `${query.toUpperCase()}`}</p>
         </div>
         <hr />
         <div className="continent">
           <h1>Continente:</h1>
-          <p>{ !continent ? "NENHUM" : `${continent.toUpperCase()}` }</p>
+          <p>{!continent ? "NENHUM" : `${continent.toUpperCase()}`}</p>
         </div>
         <hr />
-        <div className="country">
+        <div className="containerCountry">
           <h1>País:</h1>
-          <p>{ !country ? "NENHUM" : `${country.toUpperCase()}`}</p>
+          <div className="country">
+            <img src={`https://countryflagsapi.com/png/${flag}`} alt="" />
+            <p>{!country ? "NENHUM" : `${country.toUpperCase()}`}</p>
+          </div>
         </div>
         <hr />
       </ResultSearch>
     </SearchStyle>
-  )
-}
+  );
+};
 
 export default Search;
