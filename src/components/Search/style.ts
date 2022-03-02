fe7aa6e12bd9031d0c37a814cdaef37a409e50e2
Gugahnstn <gustavonstn@hotmail.com>
2022-03-02 17:15:45 -0300

    fix: changing Styled.div to Styled.form.

--- src/components/Search/style.ts
+++ src/components/Search/style.ts
@@ -8,30 +8,30 @@ export const SearchStyle = styled.div`
 export const SearchText = styled.div`
   font-family: var(--roboto-mono), monospace;
   text-align: center;
-  font-size: 30px;
+  font-size: 1.875rem;
 
-  margin-top: 20px;
+  margin-top: 1.25rem;
 `;
 
-export const SearchInput = styled.div`
+export const SearchInput = styled.form`
   justify-content: center;
   align-items: center;
   margin-top: 2.5rem;
   display: flex;
-  gap: 8px;
+  gap: 0.5rem;
 
   .search {
     padding: 0.625rem 20rem 0.625rem 0.75rem;
 
     font-family: var(--roboto), monospace;
-    font-size: 18px;
+    font-size: 1.125rem;
 
     border-color: #3f3d56b3;
     border-style: solid;
-    border-radius: 5px;
-    border-width: 3px;
+    border-radius: 0.3125rem;
+    border-width: 0.1875rem;
+    font-size: 1.125rem;
     font-weight: 700;
-    font-size: 18px;
 
     color: #000;
 
@@ -43,15 +43,13 @@ export const SearchInput = styled.div`
     background-color: #1B9105;
 
     font-family: var(--roboto-slab), monospace;
-    font-size: 20px;
+    font-size: 1.25rem;
 
+    border-radius: 0.3125rem;
     border-style: none;
-    border-radius: 5px;
 
     cursor: pointer;
-
     outline: none;
-
     color: #fff;
   }
 
@@ -89,13 +87,24 @@ export const ResultSearch = styled.div`
   p {
     font-family: var(--roboto-slab), monospace;
     font-weight: 700;
-    font-size: 28px
+    font-size: 1.75rem
   }
   
   .cityEstate, 
   .query, 
   .continent, 
-  .country {
+  .containerCountry {
     margin: 0 auto;
+  }
+  
+  .country {
+    align-items: center;
+    display: flex;
+  }
+
+  .country > img { 
+    border-radius: 0.3125rem;
+    margin-right: 0.4rem;
+    width: 2.81252rem;
   }
 `;
