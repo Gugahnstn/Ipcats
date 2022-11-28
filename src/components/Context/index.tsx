dffab0136e8587fc226a9d7e8fee21557686b676
Gustavo Nogueira <gustavonstn@hotmail.com>
2022-11-28 03:54:05 -0300

    feat: adaptando a nova forma de importação dos componentes de estilização

--- src/components/Context/index.tsx
+++ src/components/Context/index.tsx
@@ -1,18 +1,20 @@
-import { ContextStyle, ContextStyleText , ContextStyleImage } from "./style"
-import WorldConnection from "./image/World_Connection.svg"
+import WorldConnection from "/public/image/World_Connection.svg";
+import * as S from "./style";
 
 const Context = () => {
-  return(
-    <ContextStyle>
-      <ContextStyleText>
-        <h1>SITE PARA BUSCA DE IP E DOMINIO.</h1>
-        <p>Criado com o intuito de buscar por Ip de maquinas e dominios de sites.</p>
-      </ContextStyleText>
-      <ContextStyleImage>
-        <img src={WorldConnection} alt="Mundo Conectado"/>
-      </ContextStyleImage>
-    </ContextStyle>
+  return (
+    <S.ContextStyle>
+      <S.ContextStyleText>
+        <S.Title>SITE PARA BUSCA DE IP E DOMINIO.</S.Title>
+        <S.Phrase>
+          Criado com o intuito de buscar por Ip de maquinas e dominios de sites.
+        </S.Phrase>
+      </S.ContextStyleText>
+      <S.ContextStyleImage>
+        <img src={WorldConnection} alt="Imagem representando um mundo conectado" />
+      </S.ContextStyleImage>
+    </S.ContextStyle>
   );
-}
+};
 
 export default Context;
