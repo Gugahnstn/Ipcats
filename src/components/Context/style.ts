0d0ccab098e9a3b430455351f06bb5b3d9749b65
Gustavo Nogueira <gustavonstn@hotmail.com>
2022-11-28 03:54:49 -0300

    feat: deixando comentarios de media query

--- src/components/Context/style.ts
+++ src/components/Context/style.ts
@@ -3,24 +3,33 @@ import styled from "styled-components"
 export const ContextStyle = styled.div`
   align-items: center;
   display: flex;
+
+  /* @media (max-width: 800px) {
+    flex-direction: column;
+    display: block;
+  } */
 `;
 
 export const ContextStyleText = styled.div`
-
-  h1 {
-    font-family: monospace;
-    margin-bottom: 1rem;
-    font-weight: 900;
-    font-size: 3rem;
-    width: 30rem;
-  }
-
-  p {
-    font-family: monospace;
-    font-weight: 500;
-    font-size: 1.5rem;
-  }
+  /* @media (max-width: 800px) {
+    text-align: center;
+  } */
 `;
+
+
+export const Title = styled.h1`
+  font-family: monospace;
+  margin-bottom: 1rem;
+  font-weight: 900;
+  font-size: 3rem;
+  width: 30rem;
+`
+
+export const Phrase = styled.p`
+  font-family: monospace;
+  font-size: 1.5rem;
+  font-weight: 500;
+`
 
 export const ContextStyleImage = styled.div`
   img {
