dc97ab0de90a4ce0205aea17addab7a19ae3b4e9
gugahnstn <gustavonstn@hotmail.com>
2023-01-01 08:30:39 -0300

    feat: adicionando ThemeProvider no App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { GlobalStyle } from "./style";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/style";
+
+import theme from "./styles/theme";
+
+import Hero from "./templates/Hero";
 import Home from "./pages/Home";
+
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
 
