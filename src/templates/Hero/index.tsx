00d9ebffe6d8ddbf0409a6237e14bc6fad98e9c6
gugahnstn <gustavonstn@hotmail.com>
2023-01-01 08:35:38 -0300

    refactor: adicionando componente Hero na pasta templates.

---
+++ src/templates/Hero/index.tsx
@@ -0,0 +1,24 @@
+import worldConnection from '../../../public/images/worldconnection.svg';
+
+import { Container } from '../../styles/style';
+import * as S from './style';
+
+const Hero = () => {
+  return (
+    <S.HeroComponent>
+      <Container>
+        <S.HeroContainer>
+          <S.HeroTextContainer>
+            <S.HeroTitle>As Informações que você precisa na palma da sua mão.</S.HeroTitle>
+            <S.HeroPhrase>Consiga informações de Ips e dominios de um forma simples e eficiente sem demandar tempo e sim de uma forma rápida de conseguir informaçòes, sem problemas para ter o que você quer.</S.HeroPhrase>
+          </S.HeroTextContainer>
+          <S.HeroImageContainer>
+            <S.Image src={worldConnection} alt="" />
+          </S.HeroImageContainer>
+        </S.HeroContainer>
+      </Container>
+    </S.HeroComponent>
+  );
+}
+
+export default Hero;
