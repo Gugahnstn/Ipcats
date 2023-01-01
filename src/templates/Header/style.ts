308e697c7ca80a1cdf72c40c49ad0e734e215539
gugahnstn <gustavonstn@hotmail.com>
2023-01-01 08:34:20 -0300

    refactor: adicionando estilização do Header com responsividade.

---
+++ src/templates/Header/style.ts
@@ -0,0 +1,34 @@
+import styled, { css } from "styled-components";
+
+export const HeaderComponent = styled.header`
+  ${({ theme }) => css`
+    box-shadow: 0px 4px 4px rgba(81, 42, 18, 0.1);
+    background-color: ${theme.colors.background};
+    font-family: ${theme.font.Inter};
+    position: fixed;
+    width: 100%;
+    right: 0px;
+    top: 0px;
+  `};
+`;
+
+export const ContainerHeader = styled.div`
+  justify-content: space-between;
+  padding: 1.5rem 1rem;
+  align-items: center;
+  display: flex;
+  gap: 0.625rem;
+`;
+
+export const Image = styled.img`
+  width: 116px;
+`;
+
+export const TextIp = styled.p`
+  line-height: 1rem;
+  color: #ffff;
+`;
+
+export const SpanTextIp = styled.span`
+  text-decoration: underline;
+`;
