550765f43419537f1f93b3d8530873f28924f6d3
gugahnstn <gustavonstn@hotmail.com>
2023-01-01 08:33:44 -0300

    refactor: adicionando componente Header na pasta Template.

---
+++ src/templates/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from "../../../public/images/Logo.svg";
+
+import { Container } from "../../styles/style";
+import * as S from "./style";
+
+const Header = () => {
+  return (
+    <S.HeaderComponent>
+      <Container>
+        <S.ContainerHeader>
+          <S.Image src={logo} />
+          <S.TextIp>
+            IP: <S.SpanTextIp>000.000.000</S.SpanTextIp>
+          </S.TextIp>
+        </S.ContainerHeader>
+      </Container>
+    </S.HeaderComponent>
+  );
+};
+
+export default Header;
