4bd4d08212488f825def785471b6549da12f3772
gugahnstn <gustavonstn@hotmail.com>
2023-01-03 01:26:11 -0300

    feat: removendo padding, adicionando webkit de scrollbar para modificar a scrollbar.

---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import styled, { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: ${theme.font.Inter};
+      background-color: ${theme.colors.background};
+    }
+
+    ::-webkit-scrollbar {
+      height: 0rem;
+      width: 6px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: ${theme.colors.primary};
+    }
+  `} 
+`;
+
+export const Container = styled.div`
+  @media (min-width: 1280px) {
+    margin: 0 auto;
+    width: 1280px;
+  }
+`;
