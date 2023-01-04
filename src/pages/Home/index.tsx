a1bec32d57d2a6809c3db94f2862a0a0e2ad26a9
gugahnstn <gustavonstn@hotmail.com>
2023-01-04 00:07:14 -0300

    feat: acrescentando o componente 'Main' a página do site.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../../templates/Header';
 import Hero from '../../templates/Hero';
+import Main from '../../templates/Main';
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Main />
     </>
   );
 }
