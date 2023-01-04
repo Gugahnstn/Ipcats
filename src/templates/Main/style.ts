0538170cfab7da9fdd4621dbca118546cca12f9a
gugahnstn <gustavonstn@hotmail.com>
2023-01-04 00:01:28 -0300

    feat: adicionando estilização do componente 'MainContainer'.

--- src/templates/Main/style.ts
+++ src/templates/Main/style.ts
@@ -1,5 +1,22 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const MainComponent = styled.main`
+  padding: 2rem 0rem;
+`;
 
+export const MainContainer = styled.div`
+`
+
+export const Phrase = styled.p`
+  ${({ theme }) => css`
+    font-size: 1.5rem;
+    text-align: center;
+    margin-right: 2rem;
+    color: ${theme.colors.text};
+
+    @media (max-width: 860px) {
+      margin-right: 0rem;
+      font-size: 1rem;
+    }
+  `}
 `;
