72a237040a54614e7be9330aa94dc88228468df2
gugahnstn <gustavonstn@hotmail.com>
2023-01-05 21:51:56 -0300

    feat: adicionando vários componentes 'CardResult' ao componente Main.

--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,19 +1,26 @@
+import CardResult from "../../components/CardResult";
 import Search from "../../components/Search";
 
 import { Container } from "../../styles/global";
 import * as S from "./style";
 
 const Main = () => {
-  return(
+  return (
     <S.MainComponent>
       <Container>
         <S.MainContainer>
           <S.Phrase>DIGITE O IP/DOMINIO DESEJADO:</S.Phrase>
           <Search />
+          <S.ResultComponent>
+            <CardResult title="Cidade, Estado:" phrase="OFFLINE" />
+            <CardResult title="IP/Dominio:" phrase="OFFLINE" />
+            <CardResult title="Continente:" phrase="OFFLINE" />
+            <CardResult title="País:" phrase="OFFLINE" />
+          </S.ResultComponent>
         </S.MainContainer>
       </Container>
     </S.MainComponent>
   );
-}
+};
 
 export default Main;
