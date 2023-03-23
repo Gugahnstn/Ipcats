a05baaea7a585f77aa91860c0084df736a98afef
gugahnstn <gustavonstn@hotmail.com>
2023-03-23 20:56:16 -0300

    feat: adicionando cores, familias de fontes e o tamanho de fontes ao tema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,29 @@
 const theme = {
   font: {
-    Inter: 'Inter',
+    families: {
+      font_default: "Inter",
+      font_secundary: "Montserrat",
+    },
+    sizes: {
+      text_xs: "0.75rem",
+      text_sm: "0.875rem",
+      text_base: "1rem",
+      text_lg: "1.5rem",
+      text_xl: "2rem",
+      text_2xl: "4rem",
+    },
   },
   colors: {
-    background: '#130604',
-    primary: '#FF6B10',
-    secundary: '#FFB670',
-    textInput: '#FFD4A8',
-    text: '#FFFFFF',
-  }
-}
+    background: "#130604",
+    primary: "#FF6B10",
+    secundary: "#FFB670",
+    textInput: "rgba(255, 212, 168, 0.5)",
+    lineInput: "#ACACAC",
+    borderResult: "#525151",
+    lineFooter: "#6B6B6B",
+    result: "#FF5900",
+    text: "#FFFFFF",
+  },
+};
 
 export default theme;
