fa114027bd5340ec3119a4c309dca476acfea904
gugahnstn <gustavonstn@hotmail.com>
2023-03-23 22:12:46 -0300

    styles: estilo do componente 'Hero'

--- src/templates/Hero/style.ts
+++ src/templates/Hero/style.ts
@@ -2,7 +2,6 @@ import styled, { css } from "styled-components";
 
 export const HeroComponent = styled.section`
   ${({ theme }) => css`
-    font-family: ${theme.font.Inter};
     color: ${theme.colors.text};
     padding-top: 10rem;
   `}
@@ -10,7 +9,7 @@ export const HeroComponent = styled.section`
 
 export const HeroContainer = styled.div`
   justify-content: space-between;
-  padding: 0rem 2.8rem;
+  padding: 0rem 1.5rem;
   align-items: center;
   display: flex;
 
@@ -34,37 +33,45 @@ export const HeroTextContainer = styled.div`
 export const HeroImageContainer = styled.div``;
 
 export const HeroTitle = styled.h1`
-  line-height: 4rem;
-  font-size: 4rem;
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.text_2xl};
+    line-height: ${theme.font.sizes.text_2xl};
 
-  width: 41.25rem;
+    width: 41.25rem;
 
-  @media (max-width: 860px) {
-    line-height: 2rem;
-    font-size: 2rem;
-    width: 20rem;
+    @media (max-width: 860px) {
+      line-height: ${theme.font.sizes.text_xl};
+      font-size: ${theme.font.sizes.text_xl};
+      width: 20rem;
 
-    margin: 0 auto;
-  }
+      margin: 0 auto;
+    }
+  `}
 `;
 
 export const HeroPhrase = styled.p`
-  text-align: justify;
-  font-size: 0.75rem;
-  font-weight: 600;
-  color: #BA9D9D;
-  width: 30.5rem;
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.text_base};
+    text-align: justify;
+    font-weight: 500;
+    color: #ba9d9d;
+    width: 35rem;
 
-  @media (max-width: 860px) {
-    text-align: center;
-    margin: 0 auto;
-    width: 20rem;
-  }
+    @media (max-width: 860px) {
+      font-size: ${theme.font.sizes.text_xs};
+      text-align: center;
+      margin: 0 auto;
+      width: 20rem;
+    }
+  `};
 `;
 
 export const Image = styled.img`
+  height: 458.2px;
+  width: 524px;
+
   @media (max-width: 860px) {
     height: 279.24px;
     width: 298.5px;
   }
-`
+`;
