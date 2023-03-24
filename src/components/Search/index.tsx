ec9de83ec80689f54c86b8469929d57919df3484
gugahnstn <gustavonstn@hotmail.com>
2023-03-23 22:34:16 -0300

    feat: adicionando icone do React Icon no butão de busca.

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,20 +1,24 @@
-import useSearch from "../../modules/hooks/use-search";
+import { BiSearchAlt2 } from "react-icons/all";
 
+import useSearch from "../../hooks/use-search";
 import * as S from "./style";
 
 const Search = () => {
-  const { handleSubmit, handleText, search} = useSearch();
+  const { handleSubmit, handleText, search, data } = useSearch();
+  const { innerWidth: width } = window;
 
   return (
     <S.SearchComponent>
       <S.SearchForm onSubmit={handleSubmit}>
         <S.SearchInput
-          placeholder="IP/DOMAIN"
+          placeholder="Enderenço de Ip ou Dominio"
           onChange={handleText}
           value={search}
           type="text"
         />
-        <S.SearchButton type="submit">BUSCAR</S.SearchButton>
+        <S.SearchButton type="submit">
+          {width <= 860 ? <BiSearchAlt2 size={24} /> : "BUSCAR"}
+        </S.SearchButton>
       </S.SearchForm>
     </S.SearchComponent>
   );
