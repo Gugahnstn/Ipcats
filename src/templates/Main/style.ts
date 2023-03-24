6bc4e77344765df4f0afcc55dd95c7f1dd05c5b0
gugahnstn <gustavonstn@hotmail.com>
2023-03-23 22:11:33 -0300

    styles: estilo do componente 'Main'

--- src/templates/Main/style.ts
+++ src/templates/Main/style.ts
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const MainComponent = styled.main`
-  padding: 2rem 0rem;
+  padding: 3rem 0rem;
 `;
 
 export const MainContainer = styled.div`
@@ -9,10 +9,10 @@ export const MainContainer = styled.div`
 
 export const Phrase = styled.p`
   ${({ theme }) => css`
+    color: ${theme.colors.text};
     font-size: 1.5rem;
     text-align: center;
     margin-right: 2rem;
-    color: ${theme.colors.text};
 
     @media (max-width: 860px) {
       margin-right: 0rem;
@@ -25,9 +25,10 @@ export const ResultComponent = styled.div`
   justify-content: space-between;
   align-items: center;
   display: flex;
+  gap: 0.5rem;
 
   @media (max-width: 980px) {
-    padding: 0rem 4rem;
+    padding: 0rem 2rem;
     display: block;
   }
 `
