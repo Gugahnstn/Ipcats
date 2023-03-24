e5507adca8264e458d480182fa9ad9b4d47137f4
gugahnstn <gustavonstn@hotmail.com>
2023-03-23 22:35:03 -0300

    styles: estilização do componente 'Search'

--- src/components/Search/style.ts
+++ src/components/Search/style.ts
@@ -14,27 +14,32 @@ export const SearchForm = styled.form`
 
 export const SearchInput = styled.input`
   ${({ theme }) => css`
-    border-color: ${theme.colors.secundary};
+    border-color: ${theme.colors.lineInput};
     background: transparent;
-    border-radius: 0.4rem;
-    border-width: 0.2rem;
+    border-width: 0.125rem;
+    border-radius: 0.2rem;
     border-style: solid;
     align-items: center;
 
     margin-right: 0.6rem;
 
-    color: ${theme.colors.textInput};
-    padding: 0.6rem 1rem;
-    font-family: "Inter";
+    color: ${theme.colors.text};
+    padding: 0.8rem 0.6rem;
+    font-weight: 700;
     font-size: 1rem;
-    width: 572px;
+    width: 35.75rem;
 
     :focus {
       outline: none;
     }
+
+    ::placeholder {
+      font-size: ${theme.font.sizes.text_sm};
+      color: ${theme.colors.textInput};
+    }
   
     @media (max-width: 860px) {
-      width: 272px;
+      width: 17rem;
     }
   `}
 `;
@@ -43,7 +48,7 @@ export const SearchButton = styled.button`
   ${({ theme }) => css`
     background-color: ${theme.colors.primary};
     border-radius: 0.25rem;
-    padding: 0.8rem 2rem;
+    padding: 1rem 2rem;
     border-style: none;
 
     font-size: 1rem;
@@ -51,15 +56,14 @@ export const SearchButton = styled.button`
     color: ${theme.colors.text};
 
     :hover {
-      
       filter: brightness(.9);
       cursor: pointer;
     }
 
-    :cli
-
     @media (max-width: 860px) {
       padding: 0.8rem 1rem;
+      align-items: center;
+      display: flex;
     }
   `}
 `;
