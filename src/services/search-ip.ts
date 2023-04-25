17dbff3b0b26f5f661eebb0d51ca61e7d7f2cb17
gugahnstn <gustavonstn@hotmail.com>
2023-04-25 01:48:19 -0300

    feat: puxando dados do query

--- src/services/search-ip.ts
+++ src/services/search-ip.ts
@@ -2,7 +2,7 @@ async function searchIp() {
   try{
     const response = await fetch('https://my-ip-address.bohr.io/api');
     const data = await response.json();
-    return data;
+    return data.query;
   } catch(error) {
     console.error(error);
   }
