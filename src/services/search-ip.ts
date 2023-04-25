17245b236a641eda49a3c5c68126c25c2b4e0f08
gugahnstn <gustavonstn@hotmail.com>
2023-04-25 01:21:27 -0300

    feat: adicionando uma função assíncrona para requisitar o ip do usuario atraves de uma api.

---
+++ src/services/search-ip.ts
@@ -0,0 +1,11 @@
+async function searchIp() {
+  try{
+    const response = await fetch('https://my-ip-address.bohr.io/api');
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+export default searchIp;
