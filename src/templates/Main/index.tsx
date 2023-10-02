17fd7f0d56b464261c21c334e118d72e008c1d8d
gugahnstn <gustavonstn@hotmail.com>
2023-10-02 00:34:02 -0300

    fix: removing the 'SearchMap' file and adding 'index.tsx'.

--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,30 +1,29 @@
-import { useEffect } from "react";
 import CardResult from "../../components/CardResult";
 import Search from "../../components/Search";
 
-import useSearch from "../../hooks/use-search";
+import { SearchMap } from "../../components/SearchMap";
 import { Container } from "../../styles/global";
+import useSearch from "../../hooks/use-search";
 import * as S from "./style";
 
 const Main = () => {
   const { data } = useSearch();
 
-  useEffect(() => {
-    console.log("Dados:", data);
-  }, [data]);
-  
   return ( 
     <S.MainComponent>
       <Container>
-        <S.MainContainer>
           <S.Phrase>DIGITE O IP/DOMINIO DESEJADO:</S.Phrase>
           <Search />
+        <S.MainContainer>
           <S.ResultComponent>
             <CardResult title="Cidade, Estado:" phrase={data.city+'-'+data.state} />
             <CardResult title="Ip ou dominio:" phrase={data.reverse || data.query} />
             <CardResult title="Continente:" phrase={data.continent} />
             <CardResult title="País:" flag={data.flag} phrase={data.country} />
           </S.ResultComponent>
+          <S.ContainerSearchMap>
+            <SearchMap longitude={data.lon} latitude={data.lat}/>
+          </S.ContainerSearchMap>
         </S.MainContainer>
       </Container>
     </S.MainComponent>
