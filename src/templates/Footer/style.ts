429be78a8e1c041f02800e36869ccffcc91a3c2f
gugahnstn <gustavonstn@hotmail.com>
2023-10-02 00:29:36 -0300

    feat: adding style to the Footer.

---
+++ src/templates/Footer/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const FooterComponent = styled.footer`
+  align-items: center;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+`;
+
+export const FooterTopComponent = styled.div`
+  border-bottom: 1px #6B6B6B solid;
+  justify-content: space-between;
+  padding-bottom: 0.5rem;
+  padding: 0rem 0.5rem;
+  display: flex;
+`;
+
+export const IconsComponent = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const Links = styled.a`
+  text-decoration: none;
+`;
+
+export const Icons = styled.img`
+  width: 2.6875rem;
+  height: 2.625rem;
+`
+
+export const FooterBottomComponent = styled.div`
+  padding-top: 0.5rem;
+  text-align: center;
+  color: #FFFFFF;
+`;
